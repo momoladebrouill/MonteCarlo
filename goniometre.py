749e8510d17d84bb6971cc2b941e9454277df2d2
momoladebrouill <momoladebrouill@gmail.com>
2023-03-06 12:51:54 +0100

    clean :santa:

---
+++ goniometre.py
@@ -0,0 +1,5 @@
+from monte_carlo import MonteCarlo
+
+a = MonteCarlo(5,1e-5,500)
+
+print(a)
--- monte_carlo.py
+++ monte_carlo.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy.random as rd  # sous-module numpy pour générer des tableaux de nbres aléatoire
 
 squared = lambda x:x*x
+is_number = lambda x: type(x)==int or type(x)==float
 
 class Grand:
     def __init__(self, valeur, delta):
@@ -16,7 +17,7 @@ class Grand:
             r = Grand(self.v+other.v, 0)
             r.u = sqrt(self.u * self.u + other.u * other.u)
             return r
-        elif type(other)==float or type(other)==int:
+        elif is_number(other):
             r = Grand(self.v+other,0)
             r.u = abs(other) * self.u
             return u
@@ -49,24 +50,44 @@ class MonteCarlo:
         self.n = n
         self.sim = self.truev+rd.uniform(-d,d,n)
 
-    def __sub__(self,other):
+    def montecarlotte(self,other, fun):
         if type(self)==type(other):
-            r = MonteCarlo(self.b,other.v,self.d,1)
-            r.sim = self.sim - other.sim
+            r = MonteCarlo(fun(self.truev,other.truev),self.d,1)
+            r.sim = fun(self.sim, other.sim)
             return r
+        elif is_number(other):
+            return MonteCarlo(fun(self.truev,other),self.d,self.n)
         else:
-            return MonteCarlo(self.v-other,self.d,self.n)
+            raise ArithmeticError
+
+    def __add__(self,other):
+        return self.montecarlotte(other,(lambda x,y : x + y))
     
-    @property()
+    def __sub__(self,other):
+        return self.montecarlotte(other,(lambda x,y : x - y))
+    
+    def __mul__(self,other):
+        return self.montecarlotte(other,(lambda x,y : x * y))
+    
+    def __div__(self,other):
+        return self.montecarlotte(other,(lambda x,y : x / y))
+
+
+    __radd__=__add__
+    __rsub__=__sub__
+    __rdiv__=__div__
+    __rmul__=__mul__
+    
+    @property
     def v(self):
         return np.average(self.sim)
     
-    @property()
+    @property
     def u(self):
         return np.std(self.sim,ddof=1)
     
     def __repr__(self):
-        return f"{round(self.v,10)}±{round(self.u,11)"
+        return f"{round(self.v,10)}±{round(self.u,11)}"
 
 
 
@@ -85,7 +106,7 @@ print(
 )
 t1_sim = MonteCarlo(t1.v,t1.d,N)
 t2_sim = MonteCarlo(t2.v,t2.d,N)
-T_sim = t1_sim - t2_sim
+T_sim = t2_sim - t1_sim - t2_sim+t1_sim*2
 print(
       "Résultat obtenu par simulation Monte Carlo : T ",
       T_sim,
