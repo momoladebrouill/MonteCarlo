eaf794c5190f9b516957ad2476014bc4cd66a343
momoladebrouill <momoladebrouill@gmail.com>
2023-03-07 14:58:11 +0100

    TP:terminer

--- goniometre.py
+++ goniometre.py
@@ -1,7 +1,16 @@
 from monte_carlo import MonteCarlo
+from math import sin
 
-a1 = MonteCarlo(159.27,1,500)
-a2 = MonteCarlo(118.10,1,500)
-da = a1 - a2
+N = 500
 
-print(a)
+alpha1 = MonteCarlo(2.78,0.01,N)
+alpha2 = MonteCarlo(2.06,0.01,N)
+
+deltaa = alpha1 - alpha2
+dm = deltaa / 2
+
+p = 1
+l = 589e-9
+a = 2 * p * l / (dm/2).apply(sin) 
+
+print(f'dm = {dm}rad,\n a = {a}m')
--- monte_carlo.py
+++ monte_carlo.py
@@ -3,28 +3,60 @@ from math import sqrt
 import matplotlib.pyplot as plt
 import numpy.random as rd  
 
+grec ={"a":"α",
+       "b":"β", 
+       "g":"γ", 
+       "D":"Δ",
+       "d":"δ",
+       "e":"ε",
+       "z":"ζ",
+       "h":"η",
+       "T":"Θ",
+       "t":"θ",
+       "i":"ι", 
+       "k":"κ", 
+       "L":"Λ",
+       "l":"λ",
+       "m":"μ",
+       "n":"ν", 
+       "Xi":"Ξ", 
+       "xi":"ξ",
+       "o":"ο", 
+       "P":"Π",
+       "p":"π",
+       "r":"ρ", 
+       "S":"Σ",
+       "s":"σ",
+       "t":"τ", 
+       "y":"υ", 
+       "F":"Φ",
+       "f":"φ",
+       "x":"χ", 
+       "Psi":"Ψ", 
+       "psi":"ψ", 
+       "O":"Ω",
+       "omega":"ω"}
+
 squ = lambda x : x * x
 is_number = lambda x: type(x) == int or type(x) == float
 u_prod = lambda a,b,res : res.v * sqrt(squ(a.u/a.v)+squ(b.u/b.v))
 u_som = lambda a,b,res : sqrt(squ(a.u) + squ(b.u))
-class Grand:
 
+class Grand:
     def __init__(self, valeur, delta):
         self.v = valeur
         self.d = abs(delta)
         self.u = delta / sqrt(3)
-
     def __abs__(self):
         return Grand(abs(self.v),self.d)
-
     def evaluate(self,other,fun,ufun):
-        if type(other) == type(self):
-            r = Grand(fun(self.v,other.v),self.d)
-        elif is_number(other):
-            r = Grand(fun(self.v,other.v),self.d)
-            other.u = 0
-        else:
+        if is_number(other):
+            other = Grand(other,0)
+        
+        elif type(other) != type(self):
             raise ArithmeticError
+        
+        r = Grand(fun(self.v,other.v),self.d)
         r.u = ufun(self,other,r)
         return r
     
@@ -37,7 +69,7 @@ class Grand:
     def __mul__(self, other):
         return self.evaluate(other,lambda x,y : x*y,u_prod)
     
-    def __div__(self, other):
+    def __truediv__(self, other):
         return self.evaluate(other,lambda x,y : x/y,u_prod)
 
     def __repr__(self):
@@ -48,13 +80,15 @@ class Grand:
 
     __rmul__=__mul__
     __radd__=__add__
+    __rtruediv__=__truediv__
 
 class MonteCarlo:
+    
     def __init__(self,v,d,n):
         self.truev = v
         self.d = d
         self.n = n
-        self.sim = self.truev+rd.uniform(-d,d,n)
+        self.sim = v + rd.uniform(-d,d,n)
 
     def montecarlotte(self,other, fun):
         if type(self)==type(other):
@@ -62,28 +96,37 @@ class MonteCarlo:
             r.sim = fun(self.sim, other.sim)
             return r
         elif is_number(other):
-            return MonteCarlo(fun(self.truev,other),self.d,self.n)
+            return self.apply(lambda x : fun(x,other))
+
         else:
             raise ArithmeticError
+
+    def apply(self,fun):
+        r = MonteCarlo(fun(self.truev),self.d,self.n)
+        r.sim = [fun(x) for x in self.sim]
+        return r
 
     def __add__(self,other):
         return self.montecarlotte(other,(lambda x,y : x + y))
     
-    def __sub__(self,other):
-        return self.montecarlotte(other,(lambda x,y : x - y))
-    
     def __mul__(self,other):
         return self.montecarlotte(other,(lambda x,y : x * y))
     
-    def __div__(self,other):
+    def __sub__(self,other):
+        return self.montecarlotte(other,(lambda x,y : x - y))
+    
+    def __truediv__(self,other):
         return self.montecarlotte(other,(lambda x,y : x / y))
-
+    
+    def __rsub__(self,other):
+        return self.montecarlotte(other,(lambda x,y : y - x))
+    
+    def __rtruediv__(self,other):
+        return self.montecarlotte(other,(lambda x,y : y / x))
 
     __radd__=__add__
-    __rsub__=__sub__
-    __rdiv__=__div__
     __rmul__=__mul__
-    
+        
     @property
     def v(self):
         return np.average(self.sim)
@@ -94,27 +137,3 @@ class MonteCarlo:
     
     def __repr__(self):
         return f"{round(self.v,10)}±{round(self.u,11)}"
-
-
-
-N = 10000  # nombre de simulations
-
-# Pour T ----------------------------------
-print("Pour T:------")
-# intervalles de mesure delta(l'incertitude-type associée est obtenue en divisant cet intervalle par racine de 3)
-t1 = Grand(0.0,.2e-6)
-t2 = Grand(25e-6,0.12e-6)
-T = t2 + (-1*t1)
-print(
-    "Résultat obtenu par les formules : T = ",
-    T,
-    "s"
-)
-t1_sim = MonteCarlo(t1.v,t1.d,N)
-t2_sim = MonteCarlo(t2.v,t2.d,N)
-T_sim = t2_sim - t1_sim - t2_sim+t1_sim*2
-print(
-      "Résultat obtenu par simulation Monte Carlo : T ",
-      T_sim,
-      "s"
-)
